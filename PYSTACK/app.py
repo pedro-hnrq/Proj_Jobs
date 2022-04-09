31695b1c26ed6db3e6c25fd5745371763e8db68f
PEDRO HENRIQUE SOARES FEITOSA <pedr.fts@gmail.com>
2022-04-09 15:13:29 -0300

    PyStack Week 3.0

---
+++ PYSTACK/app.py
@@ -0,0 +1,80 @@
+from funcoes import soma
+# nome = input("Digite seu nome: ")
+# n1 = int(input('Digite seu 1 número: '))
+# n2 = int(input('Digite seu 2 número: '))
+
+# b1 = not 5==3
+
+# print(f"O seu nome é {nome}")
+# print(f"Calculando: {n1+n2}")
+# print(type(nome))
+# print(b1)
+
+# idade = int(input('Digite sua idade: '))
+# media = int(input('Digite a média da sua nota: '))
+# is_maior = idade >= 18 and media >= 6
+
+# is_meio = idade >=18 or media >= 7
+
+# print(is_maior, is_meio)
+
+# nota1 = int(input('Digite uma nota: '))
+# nota2 = int(input('Digite uma nota: '))
+# nota3 = int(input('Digite uma nota: '))
+
+# # media = (nota1+nota2+nota3)/3 
+
+# # if media >= 7:
+# #     print(f'Aluno Aprovado! {media}')
+# # elif media < 7 and media >= 4:
+# #     print(f'Aluno em recuperação {media}')
+# # else:
+# #     print(f'Aluno Reprovado! {media}')
+
+
+# # for i in range (0, 101, 5):
+    
+# #     print(i)
+
+# # ###Lista###
+
+# # nomes = ["Caio", "Marcos", "Pedro"]
+# # idades = [10, 20, 30, 50, 60]
+
+# # print(nomes[0], idades[4])
+
+# idades = []
+
+# while True: 
+#     idade = int(input('Digite uma idade ou -1 para sair  ')) 
+#     if idade == -1:
+#         break
+
+#     idades.append(idade)
+
+# print(idades)
+
+# # for i in idades:
+# #     print(i)
+
+# soma = 0 
+# for i in idades:
+#     soma = soma + i
+    
+# print(f"A média das idades é {soma/len(idades)}")
+
+# pessoa = {'nome': 'Caio',
+#            'idade': 50,
+#            'cidade': 'Espanha'}
+
+# for chave, valor in pessoa.items():
+#     print(f'Chave: {chave} valor: {valor}')
+
+# def soma(n1, n2): 
+#     return n1 + n2
+
+# resultado = soma (5, 3)
+# print (resultado)
+
+x = soma (5,5)
+print(x)
